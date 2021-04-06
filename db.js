041d9b57b48fff282549eecafb758a86bb960fa2
vlado <vladodjuric9@gmail.com>
2021-04-06 15:04:12 -0400

    Final

--- db.js
+++ db.js
@@ -16,6 +16,9 @@ const proConfig = process.env.DATABASE_URL; //heroku addons
 const pool = new Pool({
   connectionString:
     process.env.NODE_ENV === "production" ? proConfig : devConfig,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
